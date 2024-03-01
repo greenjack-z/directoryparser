4152e02317831403a8cffb5570f7b51472b13d96
greenjack <e.zelyanin@gmail.com>
2024-03-01 09:47:26 +0300

    add apache commons cli to parse args

--- src/main/java/com/greenjack/Main.java
+++ src/main/java/com/greenjack/Main.java
@@ -1,35 +1,61 @@
 package com.greenjack;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 2 || args[0].equalsIgnoreCase("help")) {
-            System.out.println("""
-                    usage: parser root_path file_extension [file_extension...]
-                        root_path           path from which parsing starts
-                        file_extension      file extension to find and parse pages.
-                        file_extension...   additional extensions to be parsed
-                    """);
-            return;
-        }
-        String root = args[0];
-        List<FileTypes> types = new ArrayList<>();
+        Options options = new Options();
+
+        Option path = Option.builder("p")
+                .longOpt("path")
+                .desc("root parsing directory tree")
+                .hasArg()
+                .argName("path")
+                .required()
+                .build();
+
+        Option extensions = Option.builder("e")
+                .longOpt("extensions")
+                .desc("extensions to parse in directory tree (comma separated, without preceding dot)")
+                .hasArgs()
+                .argName("extension")
+                .valueSeparator(',')
+                .required()
+                .build();
+
+        options.addOption("h", "help", false,  "show this message")
+                .addOption(path)
+                .addOption(extensions);
+
+        CommandLineParser commandLineParser = new DefaultParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
         try {
-            for (int i = 1; i < args.length; i++) {
-                types.add(FileTypes.valueOf(args[i].toUpperCase().replaceFirst("\\.", "")));
+            CommandLine commandLine = commandLineParser.parse(options, args);
+            List<FileTypes> parsedFiles = new ArrayList<>();
+            for (String extension : commandLine.getOptionValues(extensions)) {
+                parsedFiles.add(FileTypes.valueOf(extension.toUpperCase()));
             }
+            new Parser(commandLine.getOptionValue(path), parsedFiles).parse();
+
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            helpFormatter.printHelp("derectoryparser -p path -e extension [extension...]", options);
         } catch (IllegalArgumentException e) {
             System.out.println("Not supported extension type:");
             StringBuilder typeList = new StringBuilder();
             for (FileTypes type : FileTypes.values()) {
                 typeList.append(type.getExtension()).append(" ");
             }
-            System.out.println("supported extensions: " + typeList.toString());
-            return;
+            System.out.println("supported extensions: " + typeList);
         }
-
-        new Parser(root, types).parse();
     }
 }
